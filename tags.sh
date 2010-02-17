4f2f95fa48a4508761b63a51b764d9334fcac695
Travis Brown <travisb@travisbrown.ca>
2010-02-17 00:35:14 +0000

    Add support for the GNU idutils. It is basically a reverse grep for code.
    git-svn-id: svn+ssh://svnrepo/projects/configurations/ctags@3867 561c3618-0fe6-0310-b5d6-fc65c73fee30

--- tags.sh
+++ tags.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #ctags_exe="c:\documents and settings\tbrown\my documents\bin\ctags.exe"
 tags_file="${HOME}/workspace/tags"
+id_file="${HOME}/workspace/ID"
 source_files="${HOME}/workspace"
 
 ctags -f ${tags_file} -R --extra=+fq --fields=+afiksSt ${source_files}
 
+mkid -p .svn -p CVS -o ${id_file} ${source_files} 
 
