e2450f98cb34605da22a327e59169fcb7ab8fc90
Travis Brown <travisb@travisbrown.ca>
2010-02-25 00:50:13 +0000

    Add another project in Vim since I have a new location so I can share with Windows.
    Also add idutils to my tag creation script. I find it quite useful.
    
    
    git-svn-id: svn+ssh://svnrepo/projects/configurations/ctags@3873 561c3618-0fe6-0310-b5d6-fc65c73fee30

--- tags.sh
+++ tags.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #ctags_exe="c:\documents and settings\tbrown\my documents\bin\ctags.exe"
-tags_file="${HOME}/workspace/tags"
-id_file="${HOME}/workspace/ID"
-source_files="${HOME}/workspace"
+tags_file="/Volumes/UserBackup/TBrown/work/workspace/tags"
+id_file="/Volumes/UserBackup/TBrown/work/workspace/ID"
+source_files="/Volumes/UserBackup/TBrown/work/workspace"
 
 ctags -f ${tags_file} -R --extra=+fq --fields=+afiksSt ${source_files}
 
