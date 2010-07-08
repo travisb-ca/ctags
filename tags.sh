a76930105137d6a7aad41259e7e45b861e548987
Travis Brown <travisb@travisbrown.ca>
2010-07-08 16:19:35 +0000

    Rewrite the tag database creation script to take in a list of project directories from an external file. This means I can use the same script on many machines.
    git-svn-id: svn+ssh://svnrepo/projects/configurations/ctags@4115 561c3618-0fe6-0310-b5d6-fc65c73fee30

--- tags.sh
+++ tags.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+# Read in the ~/.projects file which contains on each line one directory in
+# which to create project tags and ID files.
+
 #ctags_exe="c:\documents and settings\tbrown\my documents\bin\ctags.exe"
 tags_file="/Volumes/UserBackup/TBrown/work/workspace/tags"
 id_file="/Volumes/UserBackup/TBrown/work/workspace/ID"
 source_files="/Volumes/UserBackup/TBrown/work/workspace"
 
-ctags -f ${tags_file} -R --extra=+fq --fields=+afiksSt ${source_files}
+if [ ! -e ~/.projects ]; then
+	echo "No ~/.projects file. No databases to update"
+	exit
+fi
 
-mkid -p .svn -p CVS -o ${id_file} ${source_files} 
+for directory in `cat ~/.projects`; do
+	echo "Creating databases for ${directory}"
+	ctags -f "${directory}/tags" -R --extra=+fq --fields=+afiksSt ${directory}
+	mkid -p .svn -p CVS -o ${directory}/ID ${directory} 
+done
 
