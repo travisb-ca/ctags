1e03e5301eff77567fadd9988c2eb8abddbadc24
Travis Brown <travisb@travisbrown.ca>
2010-09-03 17:07:26 +0000

    Add support for ignoring certain directories inside a project.
    git-svn-id: svn+ssh://svnrepo/projects/configurations/ctags@4262 561c3618-0fe6-0310-b5d6-fc65c73fee30

--- tags.sh
+++ tags.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Read in the ~/.projects file which contains on each line one directory in
 # which to create project tags and ID files.
+#
+# Any additional relative directories on that line are directories inside the
+# names directory which will not be indexed. eg.
+#
+# /home/travis/projects/foo annoying_directory a/r/s/t/listings
 
 #ctags_exe="c:\documents and settings\tbrown\my documents\bin\ctags.exe"
 tags_file="/Volumes/UserBackup/TBrown/work/workspace/tags"
@@ -12,9 +17,16 @@ if [ ! -e ~/.projects ]; then
 	exit
 fi
 
-for directory in `cat ~/.projects`; do
+while read directory exclusions; do
 	echo "Creating databases for ${directory}"
-	ctags -f "${directory}/tags" --exclude=.git -R --extra=+fq --fields=+afiksSt ${directory}
-	mkid -p .svn -p CVS -p .git -x lisp -o ${directory}/ID ${directory} 
-done
+    ctags_exclude=""
+    mkid_exclude=""
+    for dir in $exclusions; do
+        ctags_exclude="${ctags_exclude} --exclude=${directory}/${dir}"
+        mkid_exclude="${mkid_exclude} --prune=${directory}/${dir}"
+    done
+    echo $ctags_exclude
+	ctags -f "${directory}/tags" --exclude=.git ${ctags_exclude} -R --extra=+fq --fields=+afiksSt ${directory}
+	mkid -p .svn -p CVS -p .git -x lisp ${mkid_exclude} -o ${directory}/ID ${directory} 
+done < ~/.projects
 
