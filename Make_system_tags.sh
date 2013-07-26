e0edc35d202ad891a8d48fa42ee4a2271c93bca6
Travis Brown <travisb@travisbrown.ca>
2013-07-26 04:03:23 +0000

    Fix the location of the system includes on MacOSX with Xcode 4
    git-svn-id: svn+ssh://svnrepo/projects/configurations/ctags@6560 561c3618-0fe6-0310-b5d6-fc65c73fee30

--- Make_system_tags.sh
+++ Make_system_tags.sh
@@ -7,7 +7,7 @@ case `uname` in
 		directories="/usr/include /usr/local/include"
 		;;
 	Darwin*)
-		directories="/usr/include /Developer/SDKs"
+		directories="/usr/include /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/"
 		;;
 	*)
 		echo "Unknown system. Using defaults."
