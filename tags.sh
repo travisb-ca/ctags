a6ab836e541477b6497ff4105f2c332e94833c45
Travis Brown <travisb@travisbrown.ca>
2016-01-20 23:19:34 +0000

    Insert tags header into generated tags file.
    git-svn-id: svn+ssh://svnrepo/projects/configurations/ctags@8675 561c3618-0fe6-0310-b5d6-fc65c73fee30

--- tags.sh
+++ tags.sh
@@ -64,6 +64,13 @@ def extract_key(line):
 infile = open(sys.argv[1], 'r')
 outfile = open(sys.argv[2], 'w')
 
+outfile.write('''!_TAG_FILE_FORMAT	2	/extended format; --format=1 will not append ;" to lines/
+!_TAG_FILE_SORTED	1	/0=unsorted, 1=sorted, 2=foldcase/
+!_TAG_PROGRAM_AUTHOR	Darren Hiebert	/dhiebert@users.sourceforge.net/
+!_TAG_PROGRAM_NAME	Exuberant Ctags	//
+!_TAG_PROGRAM_URL	http://ctags.sourceforge.net	/official site/
+!_TAG_PROGRAM_VERSION	5.8	//''')
+
 lines_to_sort = []
 current_tag = ''
 
